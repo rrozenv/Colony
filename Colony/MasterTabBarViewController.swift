7f1175e5f64a2be8e1f9110c811faf385315e7bd
rrozenv <rrozenv@yahoo.com>
2017-08-27 12:21:24 -0400

    first commit

---
+++ Colony/MasterTabBarViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MasterTabBarViewController.swift
+//  Colony
+//
+//  Created by Robert Rozenvasser on 8/25/17.
+//  Copyright © 2017 GoKid. All rights reserved.
+//
+
+import Foundation
---
+++ Colony/OnboardingView.swift
@@ -0,0 +1,9 @@
+//
+//  OnboardingView.swift
+//  Colony
+//
+//  Created by Robert Rozenvasser on 8/25/17.
+//  Copyright © 2017 GoKid. All rights reserved.
+//
+
+import Foundation
